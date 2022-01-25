735a10176043c4b41ff9b08ff1786fb3fee7a5a6
Dung <dungnguyen1@beeketing.net>
2022-01-25 22:51:00 +0700

    dung update

--- src/api-services/userService.js
+++ src/api-services/userService.js
@@ -1,7 +1,6 @@
 import axiosClient from "./axiosClient";
 
-// const url = "https://ptud-dotnet-server.herokuapp.com/api/User";
-const url = "https://localhost:5001/api/User";
+const url = "https://ptud-dotnet-server.herokuapp.com/api/User";
 const userService = {
   login(payload) {
     return axiosClient.post(`${url}/Login`, { Username: payload.username, Password: payload.password});
